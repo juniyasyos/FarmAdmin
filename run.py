93476fdae774ad37888d514fb990be14a4a46878
Juniyasyos <juniyasyos@gmail.com>
2023-11-13 21:06:07 +0700

    Tambah Folder Static

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,template_folder='')
+app = Flask(__name__,template_folder='template')
 
 @app.route('/')
 def home():
@@ -10,14 +10,17 @@ def home():
 def about():
     return 'Ini adalah halaman about kami.'
 
+@app.route('/Dashboard')
+def Dashboard():
+    return render_template('public/html/Dashboard.html')
+    
 @app.route('/Homepage')
 def Homepage():
     return render_template('public/html/Homepage.html')
 
-# Handler untuk rute 404 (rute yang tidak ada dalam opsi kode)
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('public/html/nangendi.html')
+    return render_template('public/html/404.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
