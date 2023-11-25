0458adaaad61bb6c261066dcdbed202c34a451fd
Unsi236 <unsiyahmuhtaromah61820@gmail.com>
2023-11-25 17:06:45 +0700

    class User FP

--- models.py
+++ models.py
@@ -76,22 +76,30 @@ class User(db.Model, UserMixin):
     Password = db.Column(db.String(255), nullable=False)
     Bio = db.Column(db.TEXT)
 
-    def get_total_lahan(self):
-        return Lahan.query.filter_by(user_id=self.id).count()
+    # def get_total_lahan(self):
+    #     return Lahan.query.filter_by(user_id=self.id).count()
+    get_total_lahan = lambda user: sum(1 for _ in Lahan.query.filter_by(user_id=user.id))
 
-    def get_total_hasil_panen(self):
-        return Hasil_Panen.query.join(Lahan).filter(Lahan.user_id == self.id).count()
 
-    def get_total_pendapatan(self):
-        return db.session.query(func.sum(Pendapatan.harga_barang * Pendapatan.jumlah)).\
-            join(Hasil_Panen).join(Lahan).\
-            filter(Hasil_Panen.lahan_id == Lahan.id).filter(Lahan.user_id == self.id).\
-            scalar() or 0
+    # def get_total_hasil_panen(self):
+    #     return Hasil_Panen.query.join(Lahan).filter(Lahan.user_id == self.id).count()
+    get_total_hasil_panen = lambda user: sum(1 for _ in Hasil_Panen.query.join(Lahan).filter(Lahan.user_id == user.id))
 
-    def get_total_pengeluaran(self):
-        return db.session.query(func.sum(Pengeluaran.total_pengeluaran)).\
-            join(Aktivitas_Lahan, Aktivitas_Lahan.pengeluaran_id == Pengeluaran.id).\
-            join(Lahan).filter(Lahan.user_id == self.id).scalar() or 0
+
+    # def get_total_pendapatan(self):
+    #     return db.session.query(func.sum(Pendapatan.harga_barang * Pendapatan.jumlah)).\
+    #         join(Hasil_Panen).join(Lahan).\
+    #         filter(Hasil_Panen.lahan_id == Lahan.id).filter(Lahan.user_id == self.id).\
+    #         scalar() or 0
+    get_total_pendapatan = lambda user: db.session.query(func.sum(Pendapatan.harga_barang * Pendapatan.jumlah)).join(Hasil_Panen).join(Lahan).filter(Hasil_Panen.lahan_id == Lahan.id).filter(Lahan.user_id == user.id).scalar() or 0
+
+
+    # def get_total_pengeluaran(self):
+    #     return db.session.query(func.sum(Pengeluaran.total_pengeluaran)).\
+    #         join(Aktivitas_Lahan, Aktivitas_Lahan.pengeluaran_id == Pengeluaran.id).\
+    #         join(Lahan).filter(Lahan.user_id == self.id).scalar() or 0
+    get_total_pengeluaran = lambda user: db.session.query(func.sum(Pengeluaran.total_pengeluaran)).join(Aktivitas_Lahan, Aktivitas_Lahan.pengeluaran_id == Pengeluaran.id).join(Lahan).filter(Lahan.user_id == user.id).scalar() or 0
+
     
     def get_list_lahan(self):
         result = list(map(lambda x:str(x)[1:-1], Lahan.query.filter_by(user_id=self.id).all()))
@@ -197,10 +205,9 @@ class Pengeluaran(db.Model, UserMixin):
             .filter(Lahan.user_id == current_user.id)
             .all()
         )
-        sorted_result = sorted(filter(lambda item: item[0].user_id == current_user.id, query_result), key=lambda item: item[2].tanggal, reverse=True)
-    
-        # filtered_result = filter(lambda item: item[0].user_id == current_user.id, query_result) 
-        # sorted_result = sorted(filtered_result, key=lambda item: item[2].tanggal, reverse=True)
+
+        filtered_result = filter(lambda item: item[0].user_id == current_user.id, query_result)
+        sorted_result = sorted(filtered_result, key=lambda item: item[2].tanggal, reverse=True)
 
         return (list(sorted_result))
     
